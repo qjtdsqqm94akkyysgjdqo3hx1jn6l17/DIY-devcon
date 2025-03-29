12002487a34c07cd25e36a7bb1a1ec361be4f4f2
18874studentvgu <18874@student.vgu.edu.vn>
2025-03-29 18:53:39 +0700

    Add licensing

--- start.bash
+++ start.bash
@@ -1,6 +1,22 @@
 #!/bin/bash
 # shellcheck disable=SC1091,SC1091,SC2154
 
+# Script to start the REH server container
+# Copyright (C) 2025  qjtdsqqm94akkyysgjdqo3hx1jn6l17
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser Public License for more details.
+#
+# You should have received a copy of the GNU Lesser Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 CURR_DIR="$(pwd)"
 : "${run_cmd:=sh "/bin/codium-server/start-server.sh"}"
 # shellcheck source-path=./.dev-env-TEMPLATE
